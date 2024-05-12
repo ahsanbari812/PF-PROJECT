7a836ebbd01694923643fe79a097455e4d903127
Ahmedbinali1 <167882505+Ahmedbinali1@users.noreply.github.com>
2024-05-13 01:44:22 +0500

    Add files via upload

---
+++ move.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include<time.h>
+#include<ctype.h>
+#include <time.h>
+#include <windows.h>
+#include <process.h>
+
+int length;
+int bend_no;
+int len;
+char key;
+struct coordinate//There are 2 coordinate axis such as X-axis and Y-axis//
+{
+    int x;
+    int y;
+    int direction;
+};
+
+typedef struct coordinate coordinate;
+
+coordinate head, bend[500],food,body[30];
+void Move()//This function is used to move the snake//
+{
+    int a,i;
+
+    do
+    {
+
+        Food();
+        fflush(stdin);
+
+        len=0;
+
+        for(i=0; i<30; i++)
+
+        {
+
+            body[i].x=0;
+
+            body[i].y=0;
+
+            if(i==length)
+
+                break;
+
+        }
+
+        Delay(length);
+
+        Boarder();
+
+        if(head.direction==RIGHT)
+
+            Right();
+
+        else if(head.direction==LEFT)
+
+            Left();
+
+        else if(head.direction==DOWN)
+
+            Down();
+
+        else if(head.direction==UP)
+
+            Up();
+
+        ExitGame();
+
+    }
+    while(!kbhit());
+
+    a=getch();
+
+    if(a==27)
+
+    {
+
+        system("cls");
+
+        exit(0);
+
+    }
+    key=getch();
+
+    if((key==RIGHT&&head.direction!=LEFT&&head.direction!=RIGHT)||(key==LEFT&&head.direction!=RIGHT&&head.direction!=LEFT)||(key==UP&&head.direction!=DOWN&&head.direction!=UP)||(key==DOWN&&head.direction!=UP&&head.direction!=DOWN))
+
+    {
+
+        bend_no++;
+
+        bend[bend_no]=head;
+
+        head.direction=key;
+
+        if(key==UP)
+
+            head.y--;
+
+        if(key==DOWN)
+
+            head.y++;
+
+        if(key==RIGHT)
+
+            head.x++;
+
+        if(key==LEFT)
+
+            head.x--;
+
+        Move();
+
+    }
+
+    else if(key==27)
+
+    {
+
+        system("cls");
+
+        exit(0);
+
+    }
+
+    else
+
+    {
+
+        printf("\a");
+
+        Move();
+
+    }
+}
