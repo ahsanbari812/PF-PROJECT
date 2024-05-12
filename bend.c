0fbaf04ca3303579cafd4a1b88429528766ed323
Ahmedbinali1 <167882505+Ahmedbinali1@users.noreply.github.com>
2024-05-13 01:44:00 +0500

    Add files via upload

---
+++ bend.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include<time.h>
+#include<ctype.h>
+#include <time.h>
+#include <windows.h>
+#include <process.h>
+
+int length;
+int bend_no;
+int len;
+char key;
+
+struct coordinate//There are 2 coordinate axis such as X-axis and Y-axis//
+{
+    int x;
+    int y;
+    int direction;
+};
+
+typedef struct coordinate coordinate;
+
+coordinate head, bend[500],food,body[30];
+void Bend()
+{
+    int i,j,diff;
+    for(i=bend_no; i>=0&&len<length; i--)
+    {
+        if(bend[i].x==bend[i-1].x)
+        {
+            diff=bend[i].y-bend[i-1].y;
+            if(diff<0)
+                for(j=1; j<=(-diff); j++)
+                {
+                    body[len].x=bend[i].x;
+                    body[len].y=bend[i].y+j;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+            else if(diff>0)
+                for(j=1; j<=diff; j++)
+                {
+                    *GotoXY(bend[i].x,(bend[i].y-j));
+                    printf("");/
+                    body[len].x=bend[i].x;
+                    body[len].y=bend[i].y-j;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+        }
+        else if(bend[i].y==bend[i-1].y)
+        {
+            diff=bend[i].x-bend[i-1].x;
+            if(diff<0)
+                for(j=1; j<=(-diff)&&len<length; j++)
+                {
+                    *GotoXY((bend[i].x+j),bend[i].y);
+                    printf("");/
+                    body[len].x=bend[i].x+j;
+                    body[len].y=bend[i].y;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+            else if(diff>0)
+                for(j=1; j<=diff&&len<length; j++)
+                {
+                    *GotoXY((bend[i].x-j),bend[i].y);
+                    printf("");/
+                    body[len].x=bend[i].x-j;
+                    body[len].y=bend[i].y;
+                    GotoXY(body[len].x,body[len].y);
+                    printf("*");
+                    len++;
+                    if(len==length)
+                        break;
+                }
+        }
+    }
+}
